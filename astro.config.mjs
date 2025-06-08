1513239216d81561c9af0eb41bfd49ba89700448
stefanmilic017 <stefanmilic017@gmail.com>
2025-06-09 00:41:57 +0200

    implemented preact .jsx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://example.com"
+  site: "https://astro-tutorial2025.netlify.app.com",
+  integrations: [preact()]
 });
